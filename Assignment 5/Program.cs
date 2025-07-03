42d4e281252855543bcf74c32b0bfae8e9e3267d
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-07-03 18:29:41 +0300

    Q4 solved

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -45,6 +45,18 @@ namespace Assignment_5
             Console.WriteLine($"The sum of the digits of the number {num} is: {SumDigits(num)}");
             #endregion
 
+            #region Q4 Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+            static bool IsPrime(int n)
+            {
+                if (n < 2) return false;
+                if (n == 2) return true;
+                if (n % 2 == 0) return false;
+
+                for (int i = 3; i * i <= n; i += 2)
+                    if (n % i == 0) return false;
+                return true;
+            }
+            #endregion
 
 
         }
