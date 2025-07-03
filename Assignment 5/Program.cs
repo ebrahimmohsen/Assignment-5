605ce80a96b13d17ee2a16ae146e1cdfe8a1d8c1
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-07-03 18:23:24 +0300

    Q1 solved

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -9,7 +9,7 @@ namespace Assignment_5
         {
             #region Q1 Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
             //By value :A copy of the value‑type( int, double, struct) is placed on the stack and given to the method.
-            //Caller’s original variable never changes.
+            //Caller’s original variable never changes..
 
             //By reference : A reference to the caller’s variable is passed; the method works with the original storage.
             //Changes inside the method reflect outside(except in, which is read‑only).
